5c2321674d8dac0e9879ceee576ab910fd06fed6
Jose Rafael S. Matienzo <josematienzo101@gmail.com>
2025-04-11 17:42:15 +0800

    Import svg files to ensure they are included in deployment

--- src/components/CurrentWeatherInfo.jsx
+++ src/components/CurrentWeatherInfo.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import clearSkyDay from '/assets/clearSkyDay.svg';
+import clearSkyNight from '/assets/clearSkyNight.svg';
+import clouds from '/assets/clouds.svg';
+import drizzle from '/assets/drizzle.svg';
+import mist from '/assets/mist.svg';
+import rain from '/assets/rain.svg';
+import snow from '/assets/snow.svg';
+import thunderstorm from '/assets/thunderstorm.svg';
 
 function CurrentWeatherInfo({ city, country, temp, time, windSpeed, weatherId }) {
+  const iconMap = {clearSkyDay, clearSkyNight, clouds, drizzle, mist, rain, snow, thunderstorm};
   let weatherIcon = ""
   if (weatherId < 300) {
     weatherIcon = "thunderstorm"
@@ -14,21 +23,20 @@ function CurrentWeatherInfo({ city, country, temp, time, windSpeed, weatherId })
     weatherIcon = "mist"
   } else if (weatherId == 800) {
     if ((parseInt(time.substring(0, 2), 10) > 6 && time[6] == 'P') || (parseInt(time.substring(0, 2), 10) < 6 && time[6] == 'A')) {
-      weatherIcon = "clear sky night"
+      weatherIcon = "clear_sky_night"
     } else {
-      weatherIcon = "clear sky day"
+      weatherIcon = "clear_sky_day"
     }
   } else {
     weatherIcon = "clouds"
   }
-  const srcIcon = `/assets/${weatherIcon}.svg`
 
   return (
     <div className='flex flex-col w-[75%]'>
         <div className='flex'>
             <span className='text-7xl sm:text-7xl md:text-8xl lg:text-8xl xl:text-9xl font-bold'>{temp ? Math.floor(temp) : 29}</span>
             <span className='text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-semibold'>°C</span>
-            <img src={srcIcon} className='size-18 sm:size-18 md:size-19 lg:size-19 xl:size-20 ml-5 self-center'></img>
+            <img src={iconMap[weatherIcon]} className='size-18 sm:size-18 md:size-19 lg:size-19 xl:size-20 ml-5 self-center'></img>
         </div>
         <span className='text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-medium'>{city}, {country}</span>
         <div className='flex items-center mt-1'>
--- src/components/DayForecast.jsx
+++ src/components/DayForecast.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import clearSkyDay from '/assets/clearSkyDay.svg';
+import clearSkyNight from '/assets/clearSkyNight.svg';
+import clouds from '/assets/clouds.svg';
+import drizzle from '/assets/drizzle.svg';
+import mist from '/assets/mist.svg';
+import rain from '/assets/rain.svg';
+import snow from '/assets/snow.svg';
+import thunderstorm from '/assets/thunderstorm.svg';
 
 function DayForecast({day, forecast}) {
+  const iconMap = {clearSkyDay, clearSkyNight, clouds, drizzle, mist, rain, snow, thunderstorm};
   let temp = 23
-  let srcIcon = "/assets/clouds.svg"
+  let weatherIcon = ""
   if (forecast != null) {
     temp = forecast['temp']
     let time = forecast['time']
     let weatherId = forecast['weather']
-    let weatherIcon = ""
     if (weatherId < 300) {
       weatherIcon = "thunderstorm"
     } else if (weatherId < 500) {
@@ -20,20 +28,19 @@ function DayForecast({day, forecast}) {
       weatherIcon = "mist"
     } else if (weatherId == 800) {
       if ((parseInt(time.substring(0, 2), 10) > 6 && time[6] == 'P') || (parseInt(time.substring(0, 2), 10) < 6 && time[6] == 'A')) {
-        weatherIcon = "clear sky night"
+        weatherIcon = "clearSkyNight"
       } else {
-        weatherIcon = "clear sky day"
+        weatherIcon = "clearSkyDay"
       }
     } else {
       weatherIcon = "clouds"
     }
-    const srcIcon = `/assets/${weatherIcon}.svg`;
   }
 
   return (
     <div className='flex flex-col items-center'>
       <span className='text-xs sm:text-xs md:text-base lg:text-lg xl:text-xl font-bold'>{day}</span>
-      <img src={srcIcon} className='size-8 sm:size-10 md:size-11 lg:size-12 xl:size-12.5 m-2'></img>
+      <img src={iconMap[weatherIcon]} className='size-8 sm:size-10 md:size-11 lg:size-12 xl:size-12.5 m-2'></img>
       <span className='text-base sm:text-base md:text-lg lg:text-xl xl:text-xl font-medium'>{temp}°C</span>
     </div>
   )
